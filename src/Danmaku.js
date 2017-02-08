02735db22a0d41b33bde2ac73a6b48c03b39d1d7
Ben Pig Chu <benpichu@gmail.com>
2017-02-08 16:45:01 +0800

    extract the danmaku base class

---
+++ src/Danmaku.js
@@ -0,0 +1,45 @@
+/// the danmaku base class
+export class Danmaku {
+
+	constructor(renderer, content, style = {}) {
+		// basic
+		this.renderer = renderer
+		this.content = content
+
+		// style
+		this.style = Object.assign({}, this.renderer.style, style)
+
+		// get size
+		this.renderer.canvas.font = this.style.fontSize + "px Roboto, Microsoft YaHei, 黑体, 宋体, sans-serif"
+		this.renderer.canvas.textAlign = "left"
+		this.renderer.canvas.textBaseline = "top"
+		let textMeasure = this.renderer.canvas.measureText(this.content)
+		this.height = this.style.fontSize
+		this.width = textMeasure.width
+
+		// init
+		this.age = 0
+		this.x = this.renderer.element.width
+		this.y = this._getY()
+	}
+
+	/// step into next frame
+	step(time) {
+		this.age += time
+		if (this.age > this.style.time) {
+			this.renderer.danmakuPool.delete(this)
+		}
+	}
+
+	/// render danmaku into renderer
+	render() {
+		this.renderer.canvas.fillStyle = this.style.color
+		this.renderer.canvas.strokeStyle = this.style.strokeColor
+		this.renderer.canvas.lineWidth = 2
+		this.renderer.canvas.textAlign = "left"
+		this.renderer.canvas.textBaseline = "top"
+		this.renderer.canvas.font = this.height + "px Roboto, Microsoft YaHei, 黑体, 宋体, sans-serif"
+		this.renderer.canvas.strokeText(this.content, this.x, this.y)
+		this.renderer.canvas.fillText(this.content, this.x, this.y)
+	}
+}
--- src/RTLDanmaku.js
+++ src/RTLDanmaku.js
@@ -1,49 +1,11 @@
+import {Danmaku} from "./Danmaku.js"
 /// the danmaku class
-export class RTLDanmaku {
-
-	constructor(renderer, content, style = {}) {
-
-		// basic
-		this.renderer = renderer
-		this.content = content
-
-		// style
-		this.style = Object.assign({}, this.renderer.style, style)
-
-		// get size
-		this.renderer.canvas.font = this.renderer.fontSize + "px Roboto, Microsoft YaHei, 黑体, 宋体, sans-serif"
-		this.renderer.canvas.textAlign = "left"
-		this.renderer.canvas.textBaseline = "top"
-		let textMeasure = this.renderer.canvas.measureText(this.content)
-		this.height = this.style.fontSize
-		this.width = textMeasure.width
-
-		// init
-		this.age = 0
-		this.x = this.renderer.element.width
-		this.y = this._getY()
-
-	}
+export class RTLDanmaku extends Danmaku {
 
 	/// step into next frame
 	step(time) {
-		this.age += time
+		super.step(time)
 		this.x = this.renderer.element.width - (this.renderer.element.width + this.width) * this.age / this.style.time
-		if (this.age > this.style.time) {
-			this.renderer.danmakuPool.delete(this)
-		}
-	}
-
-	/// render danmaku into renderer
-	render() {
-		this.renderer.canvas.fillStyle = this.style.color
-		this.renderer.canvas.strokeStyle = this.style.strokeColor
-		this.renderer.canvas.lineWidth = 2
-		this.renderer.canvas.textAlign = "left"
-		this.renderer.canvas.textBaseline = "top"
-		this.renderer.canvas.font = this.height + "px Roboto, Microsoft YaHei, 黑体, 宋体, sans-serif"
-		this.renderer.canvas.strokeText(this.content, this.x, this.y)
-		this.renderer.canvas.fillText(this.content, this.x, this.y)
 	}
 
 	/// get the max length of danmaku that appear with the same y and will not catch this danmaku (may be negative)
@@ -56,7 +18,8 @@ export class RTLDanmaku {
 			keepOrderWhenBegin *= this.renderer.element.width
 			keepOrderWhenBegin /= this.width
 		}
-		return Math.min(keepOrderWhenBegin, keepOrderWhenEnd)
+		let maxLength = Math.min(keepOrderWhenBegin, keepOrderWhenEnd)
+		return maxLength
 	}
 
 	/// get valid Y at start
